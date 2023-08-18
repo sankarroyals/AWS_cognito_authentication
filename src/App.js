54a4f0d27a48187e00547736e1ca950a577b2f8b
Sankar Yalamandla <sankar.yalamandla@tigeranalytics.local>
2023-08-18 15:09:24 +0530

    added django

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import SignUp from './components/Signup/SignUp';
 import Login from './components/Login/Login';
 import { Account, AccountContext } from './components/ContextApi/Account';
 import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {Navigate, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import { useContext, useEffect, useState } from 'react';
-import NoPage from './components/NoPage';
 
 function App() {
   const {status} = useContext(AccountContext)
@@ -19,7 +18,7 @@ function App() {
           <Route path='/' element={<Home />} />
           {status?<Route path='/' element={<Home />} />:<Route path='/signup' element={<SignUp />} />}
           {status?<Route path='/' element={<Home />} />:<Route path='/login' element={<Login />} />}
-          <Route path='*' element={<NoPage />} />
+          <Route path='*' element={<Navigate to='/' />} />
         </Routes>
       </>
   );
